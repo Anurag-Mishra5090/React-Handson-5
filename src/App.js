51e310d19f8647d93cf877e73c01e7bd08d32172
= <=>
2023-03-04 15:00:59 +0530

    done

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import HoverComp from './Component/HoverComp'
+import PureComp from './Component/PureComp'
+const App = () => {
+  return (
+    <div>
+     <HoverComp/>
+     <hr />
+     <PureComp/>
+    </div>
+  )
+}
+
+export default App
